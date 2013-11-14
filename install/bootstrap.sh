50cc6e50167c0dc5dfa527940ac4022709810f15
Nelson da Costa <ndacosta@eleven-labs.com>
2013-11-14 10:57:53 +0100

    updated install script to match refactoring

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -2,15 +2,10 @@
 
 cd
 
-ln -s ./dotfiles/.bash_aliases
-ln -s ./dotfiles/.bash_ps1
+ln -s ./dotfiles/.bash_profile
+ln -s ./dotfiles/.bashrc
 ln -s ./dotfiles/.gitconfig
-ln -s ./dotfiles/.gitignore_global
 
-if [ -f ~/dotfiles/.bashrc ]; then
-    cat ~/dotfiles/.bashrc_install >> ~/.bashrc
-fi
-
-source ~/.bashrc
+source ~/.bash_profile
 
 exit 0
