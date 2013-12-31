1e34620c07780555c4f7d8b95faf0b538c25e2f9
Nelson da Costa <ndacosta@eleven-labs.com>
2013-12-31 15:18:26 +0100

    added command line utilities installation

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -6,6 +6,10 @@ ln -s ./dotfiles/.bash_profile
 ln -s ./dotfiles/.bashrc
 ln -s ./dotfiles/.gitconfig
 
+for cmd in htop pv tree; do
+    apt-get install "$cmd"
+done
+
 source ~/.bash_profile
 
 exit 0
