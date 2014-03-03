a18e2ecd5aa2a8a1ef65a8a30201f0de49aab128
Nelson da Costa <ndacosta@eleven-labs.com>
2014-03-03 22:47:47 +0100

    added dev env install instructions

--- bin/install.sh
+++ bin/install.sh
@@ -2,14 +2,24 @@
 
 cd
 
+apt-get install git
+apt-get install rubygems
+gem install git-up
+
 ln -s ./dotfiles/.bash_profile
 ln -s ./dotfiles/.bashrc
 ln -s ./dotfiles/.gitconfig
 
-sudo apt-get install rubygems
-sudo gem install git-up
+apt-get install apache2
+a2enmod vhost_alias rewrite
+apt-get install libapache2-mod-php5
 
-for cmd in acroread htop pv screen tree
+apt-get install mysql-server
+apt-get install php5-mysql
+
+apt-get install php5-cli php5-common php-apc php5-curl php5-intl php5
+
+for cmd in "acroread htop pv screen tree"
 do
     apt-get install "$cmd"
 done
