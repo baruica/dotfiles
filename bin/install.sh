b23c40b9d4cda2bc87770b4f00b4c51d52704173
Nelson da Costa <ndacosta@eleven-labs.com>
2014-03-03 22:27:48 +0100

    added git up install step

--- bin/install.sh
+++ bin/install.sh
@@ -6,6 +6,9 @@ ln -s ./dotfiles/.bash_profile
 ln -s ./dotfiles/.bashrc
 ln -s ./dotfiles/.gitconfig
 
+sudo apt-get install rubygems
+sudo gem install git-up
+
 for cmd in acroread htop pv screen tree
 do
     apt-get install "$cmd"
