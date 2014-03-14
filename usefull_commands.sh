67135661a42ca47f8a801ddc08c9c6cf442476e0
Nelson da Costa <ndacosta@eleven-labs.com>
2014-03-14 11:11:56 +0100

    added git search command

--- usefull_commands.sh
+++ usefull_commands.sh
@@ -1,3 +1,11 @@
 #!/bin/sh -
 
 composer show -i
+composer show -i <package>
+
+
+# git: search for a specific string
+# search all commits which contain the string "console.log" in the patch content
+# it doesn't search in the commit message or information (username, date, ...)
+# and it's only searching in the patch content and not in the lines added before and after the patch content.
+git log -S console.log --author lgiraudel --before="2013-10-01 00:00" --after="2013-06-01 00:00" -- web/js
