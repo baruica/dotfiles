5d005b5db76bb16de1ce24e75de88d0f2e99fa9e
Nelson da Costa <ndacosta@eleven-labs.com>
2014-03-19 00:15:41 +0100

    ungit install steps

--- bin/install.sh
+++ bin/install.sh
@@ -2,27 +2,41 @@
 
 cd
 
-apt-get install git
-apt-get install rubygems
-gem install git-up
+sudo apt-get install git
+sudo apt-get install rubygems
+sudo gem install git-up
+
+sudo apt-get install nodejs
+sudo apt-get install software-properties-common
+
+
+# ungit
+sudo -H npm install -g ungit
+# if  this does not work
+sudo apt-get update
+sudo apt-get install -y python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
+
 
 ln -s ./dotfiles/.bash_profile
 ln -s ./dotfiles/.bashrc
 ln -s ./dotfiles/.gitconfig
 
-apt-get install apache2
-a2enmod vhost_alias rewrite
-apt-get install libapache2-mod-php5
+sudo apt-get install apache2
+sudo a2enmod vhost_alias rewrite
+sudo apt-get install libapache2-mod-php5
 
-apt-get install mysql-server
-apt-get install php5-mysql
+sudo apt-get install mysql-server
+sudo apt-get install php5-mysql
 
-apt-get install php5-cli php5-common php-apc php5-curl php5-intl php5
+sudo apt-get install php5-cli php5-common php-apc php5-curl php5-intl php5
 
-for cmd in "acroread htop pv screen tree"
-do
-    apt-get install "$cmd"
-done
+sudo apt-get install acroread
+sudo apt-get install htop
+sudo apt-get install pv
+sudo apt-get install tree
 
 source ~/.bash_profile
 
