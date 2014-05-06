21363b98226de458b369da11ddeba3b18d431937
Nelson da Costa <ndacosta@eleven-labs.com>
2014-05-06 09:49:09 +0200

    cleanup

--- usefull_commands.sh
+++ usefull_commands.sh
@@ -4,8 +4,18 @@ composer show -i
 composer show -i <package>
 
 
+# Turn a Unix epoch time back into a human readable date
+date -d @728737200
+
+
 # git: search for a specific string
 # search all commits which contain the string "console.log" in the patch content
 # it doesn't search in the commit message or information (username, date, ...)
 # and it's only searching in the patch content and not in the lines added before and after the patch content.
 git log -S console.log --author lgiraudel --before="2013-10-01 00:00" --after="2013-06-01 00:00" -- web/js
+
+# Avoiding 'git clone' when you just need an export
+git archive --remote=<url> --format=tgz master | tar xvz
+
+# show the file changes of the x last commits
+git show --pretty=oneline --name-status HEAD~x..HEAD
