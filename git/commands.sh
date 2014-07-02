b110545463745f8b04a3dbf36650600008b09e25
Nelson da Costa <ndacosta@eleven-labs.com>
2014-07-02 10:53:26 +0200

    added git command file

---
+++ git/commands.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -
+
+# git: search for a specific string
+# search all commits which contain the string "console.log" in the patch content
+# it doesn't search in the commit message or information (username, date, ...)
+# and it's only searching in the patch content and not in the lines added before and after the patch content.
+git log -S console.log --author lgiraudel --before="2013-10-01 00:00" --after="2013-06-01 00:00" -- web/js
+
+# Avoiding 'git clone' when you just need an export
+git archive --remote=<url> --format=tgz master | tar xvz
+
+# show the file changes of the x last commits
+git show --pretty=oneline --name-status HEAD~x..HEAD
+
+# delete local branch
+git branch -D mybranch
+
+# Delete local and remote together
+git push remotename --delete :mybranch
+
+# who worked on the project
+git shortlog --numbered --summary HEAD
+#  1834  Charles-Eric Gorron
+#   859  Kieu Anh Tuan
+#   427  clement.gautier
+#   376  Clement Gautier
+#   308  qzhang
+#   212  Nelson da Costa
+
+git grep -i todo | wc -l
+# 31
