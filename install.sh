ecd95553fb9f96b3c2f1a43f436ae5ac4315d1cc
Nelson da Costa <ndacosta@eleven-labs.com>
2014-07-25 18:27:57 +0200

    added phpMyAdmin ppa install

--- install.sh
+++ install.sh
@@ -25,6 +25,10 @@ sudo apt-get install php5-mysql
 
 sudo apt-get install php5-cli php5-common php-apc php5-curl php5-intl php5
 
+sudo add-apt-repository ppa:nijel/phpmyadmin
+sudo apt-get update
+sudo apt-get install phpmyadmin
+
 # install peco, z
 sudo apt-get install acroread
 sudo apt-get install htop
