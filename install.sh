4c9ba6510515ea9dae79b3d86501af8813195f23
Nelson da Costa <ndacosta@eleven-labs.com>
2014-12-18 15:05:07 +0100

    added tig

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ sudo apt-get install ubuntu-restricted-extras
 sudo gsettings set com.canonical.Unity.Lenses disabled-scopes "['more_suggestions-amazon.scope', 'more_suggestions-u1ms.scope', 'more_suggestions-populartracks.scope', 'music-musicstore.scope', 'more_suggestions-ebay.scope', 'more_suggestions-ubuntushop.scope', 'more_suggestions-skimlinks.scope']"
 
 # install peco, z
-sudo apt-get install acroread bash-completion bleachbit curl git htop pv python-software-properties rubygems ruby-dev software-properties-common sshrc tree
+sudo apt-get install acroread bash-completion bleachbit curl git htop pv python-software-properties rubygems ruby-dev software-properties-common sshrc tig tree
 sudo gem install git-up
 
 sudo apt-get install oracle-java8-installer
