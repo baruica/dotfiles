a17fa23c203077318462ed4319196b0b83450737
Nelson da Costa <baruic.a@gmail.com>
2015-12-29 11:42:59 +0100

    fewer lines

--- install.sh
+++ install.sh
@@ -5,19 +5,12 @@ IFS=$'\n\t'
 
 cd
 
-ln -s ./dotfiles/.bash_profile
-ln -s ./dotfiles/.bashrc
-git clone https://github.com/michaeldfallen/git-radar .git-radar
-./install_client.sh
-
 sudo apt-get autoremove apport unity-lens-shopping
 sudo gsettings set com.canonical.Unity.Lenses disabled-scopes "['more_suggestions-amazon.scope', 'more_suggestions-u1ms.scope', 'more_suggestions-populartracks.scope', 'music-musicstore.scope', 'more_suggestions-ebay.scope', 'more_suggestions-ubuntushop.scope', 'more_suggestions-skimlinks.scope']"
 
-sudo apt-get install -y build-essential
-sudo apt-get install -y python-software-properties
-sudo apt-get install -y software-properties-common
-sudo apt-get install -y ubuntu-restricted-extras
+sudo apt-get install -y build-essential python-software-properties software-properties-common ubuntu-restricted-extras
 
+# Java
 sudo apt-get purge openjdk*
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
@@ -25,15 +18,14 @@ sudo apt-get install -y oracle-java8-installer
 sudo apt-get install -y oracle-java8-set-default
 
 sudo apt-get update
-
 sudo apt-get install -y bash-completion curl git git-core htop pv tree vim xclip
 sudo apt-get install -y tig
 
-sudo apt-get install rubygems-integration
-#sudo apt-get install -y ruby-dev
-sudo gem install rubygems-update
+# git-up
+sudo apt-get install -y ruby ruby-dev rubygems-integration
 sudo gem install git-up
 
+sudo gem install rubygems-update
 sudo update_rubygems
 sudo gem update --system
 
@@ -70,6 +62,11 @@ chmod +x .laptop-mode.sh && ./.laptop-mode.sh
 
 # to undo/uninstall laptop tweaks
 sudo apt-get autoremove indicator-cpufreq laptop-mode-tools && FSTABFile="noatime,nodiratime,discard,errors" && sudo sed -i -e "s/$FSTABFile/errors/g" "/etc/fstab" && sudo bash -c "echo 60 > /proc/sys/vm/swappiness" && sudo sed -i '/defaults,noatime,size=512M/d' /etc/fstab
+
+ln -s ./dotfiles/.bash_profile
+ln -s ./dotfiles/.bashrc
+git clone https://github.com/michaeldfallen/git-radar .git-radar
+./install_client.sh
 
 . ~/.bash_profile
 
