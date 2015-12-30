60b760f6599d6e85570eeeced06cb14716583ced
Nelson da Costa <baruic.a@gmail.com>
2015-12-30 10:49:39 +0100

    latest version from repo

--- symfony2-autocomplete.bash
+++ symfony2-autocomplete.bash
@@ -1,4 +1,4 @@
-#!bash
+#!/bin/bash
 #
 # bash completion support for symfony2 console
 #
@@ -25,7 +25,7 @@ foreach ($argv as $v) {
     }
 }
 
-$xmlHelp = shell_exec($script.' help --xml '.$command);
+$xmlHelp = shell_exec($script.' help --format=xml '.$command.' 2>/dev/null');
 $options = array();
 if (!$xml = @simplexml_load_string($xmlHelp)) {
     exit(0);
@@ -45,7 +45,7 @@ HEREDOC
         return 0
     fi
 
-    commands=$(${script} list --raw | sed -E 's/(([^ ]+ )).*/\1/')
+    commands=$(${script} list --raw 2>/dev/null | sed -E 's/(([^ ]+ )).*/\1/')
     COMPREPLY=($(compgen -W "${commands}" -- ${cur}))
 
     return 0;
@@ -57,4 +57,7 @@ complete -F _console console-test
 complete -F _console console-prod
 complete -F _console console-staging
 complete -F _console Symfony
+complete -F _console sf
+complete -F _console console.php
+complete -F _console composer
 COMP_WORDBREAKS=${COMP_WORDBREAKS//:}
