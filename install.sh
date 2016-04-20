6c9f946bc89301464dd909218f9eed71c0543cf6
Nelson da Costa <baruic.a@gmail.com>
2016-04-20 22:07:48 +0200

    rename git-radar local repo

--- install.sh
+++ install.sh
@@ -45,7 +45,7 @@ sudo npm install -g gnomon
 
 echo "Cleaning Up" && sudo apt-get -f install && sudo apt-get autoremove && sudo apt-get -y autoclean && sudo apt-get -y clean
 
-git clone https://github.com/michaeldfallen/git-radar ~/.git-radar
+git clone https://github.com/michaeldfallen/git-radar ~/git-radar
 
 git clone https://github.com/baruica/dotfiles ~/dotfiles
 ln -s ./dotfiles/.bash_profile
