45f771b845552bbbc83f04e285030ce9f000f6ee
Nelson da Costa <baruic.a@gmail.com>
2016-05-12 13:02:29 +0200

    shell script template with main and exit functions

--- prelude.sh
+++ prelude.sh
@@ -3,4 +3,26 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-readonly SCRIPT_DIR=$(readlink -m $(dirname $0))
+readonly SCRIPT_DIR=$(dirname $(readlink -m $0))
+
+function main
+{
+    if [ -z "$1" ] && [ -z "$2" ]; then
+        error_exit "Argument Identifiant projet attentu : bash $0 path"
+    fi
+
+    set -o errexit
+    set -o pipefail
+    set -o nounset
+    set -o errtrace
+
+    local path=$1
+}
+
+function error_exit
+{
+    echo "$1" 1>&2
+    exit 1
+}
+
+main "$@"
