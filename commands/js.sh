ceee25e1867d36ffcd512874e759d18e55dfdec3
Nelson da Costa <baruic.a@gmail.com>
2017-07-16 15:53:43 +0200

    no more yarn

--- commands/js.sh
+++ commands/js.sh
@@ -17,8 +17,3 @@ npm outdated -g --depth=0
 
 sudo npm install -g bower
 sudo npm install -g grunt-cli
-
-# yarn
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update && sudo apt install yarn
