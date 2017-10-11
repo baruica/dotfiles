142e657cf42ad5d2f6c2c3fbeb44d727dfce694a
Nelson da Costa <baruic.a@gmail.com>
2017-10-11 23:18:33 +0200

    apt install composer

--- install.sh
+++ install.sh
@@ -15,8 +15,7 @@ git config --global include.path "${DIR}"/git/.gitconfig
 git clone https://github.com/michaeldfallen/git-radar ~/git-radar
 ln -s ~/git-radar/git-radar ~/bin/git-radar
 
-sudo curl -LsS https://getcomposer.org/installer -o ~/bin/composer && sudo ln -s ~/bin/composer /usr/local/bin/composer
-sudo ln -s ~/workspace/baruica/dotfiles/composer_bash_completion.sh /etc/bash_completion.d/composer
+sudo apt install composer
 
 composer global require bamarni/symfony-console-autocomplete
 composer global require friendsofphp/php-cs-fixer
