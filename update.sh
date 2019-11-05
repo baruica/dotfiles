b0efd4a5c38f46a6bfbc16930f1c29ce10007060
Nelson da Costa <ndc@octo.com>
2019-11-05 22:50:18 +0100

    cleanup

--- update.sh
+++ update.sh
@@ -1,28 +1,19 @@
 #!/usr/bin/env bash
 
-cmd_exists()
+function cmd_exists
 {
     hash "$1" >/dev/null 2>&1
 }
 
-exec_if_cmd_exists()
+function exec_if_cmd_exists
 {
     cmd_exists "$1" && "$@"
 }
 
-sudo_exec_if_cmd_exists()
-{
-    cmd_exists "$1" && sudo "$@"
-}
-
-exec_if_cmd_exists exercism upgrade
-
-exec_if_cmd_exists composer self-update
-exec_if_cmd_exists composer global update
-
 readonly git_repos="git-radar
 z
-workspace/baruica/dotfiles"
+workspace/baruica/dotfiles
+workspace/katas/kata-starter"
 
 for git_repo in ${git_repos}; do
     if [[ -d ~/${git_repo} ]]; then
@@ -30,13 +21,20 @@ for git_repo in ${git_repos}; do
         echo ">>> ${git_repo}"
         cd ~/"${git_repo}" || exit
         git up
-        [[ -f composer.json ]] && composer update
+        [[ -f composer.json ]] && exec_if_cmd_exists composer update
     fi
 done
+
+echo
 
 if [[ -r ~/workspace/update.sh ]] && [[ -f ~/workspace/update.sh ]]; then
     sh ~/workspace/update.sh
 fi
+
+exec_if_cmd_exists exercism upgrade
+
+exec_if_cmd_exists composer self-update
+exec_if_cmd_exists composer global update
 
 sudo apt update
 sudo apt full-upgrade -y
