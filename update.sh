3d9a482b7501df67673fdd8162f992b17c65e9a3
Nelson da Costa <ndacosta@octo.com>
2020-01-25 09:57:45 +0100

    presume commands exist for better readability

--- update.sh
+++ update.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-function cmd_exists() {
-    hash "$1" >/dev/null 2>&1
-}
-
-function exec_if_cmd_exists() {
-    cmd_exists "$1" && "$@"
-}
-
 readonly git_repos="git-radar
 workspace/baruica/dotfiles"
 
@@ -18,7 +10,7 @@ for git_repo in ${git_repos}; do
         echo ">>> ${git_repo}"
         cd ~/"${git_repo}" || exit
         git up
-        [[ -f composer.json ]] && exec_if_cmd_exists composer update
+        [[ -f composer.json ]] && composer update
     fi
 done
 
@@ -28,12 +20,13 @@ if [[ -r ~/workspace/update.sh ]] && [[ -f ~/workspace/update.sh ]]; then
     sh ~/workspace/update.sh
 fi
 
-exec_if_cmd_exists exercism upgrade
-exec_if_cmd_exists composer self-update
-exec_if_cmd_exists composer global update
-exec_if_cmd_exists yes | symfony self:update
+exercism upgrade
+composer self-update
+composer global update
+yes | symfony self:update
 
 sudo apt update
 sudo apt full-upgrade -y
 
-exec_if_cmd_exists aptclean
+sudo apt clean -y
+sudo apt autoremove -y --purge
