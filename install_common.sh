0a9847c39af0597c8af7865681fce1048923b8fe
Nelson da Costa <ndacosta@octo.com>
2020-04-03 12:14:06 +0200

    install chrome-gnome-shell

--- install_common.sh
+++ install_common.sh
@@ -3,7 +3,7 @@
 sudo apt remove -y bluez bluetooth
 
 sudo add-apt-repository multiverse
-sudo apt install -y bash-completion curl git htop jq keychain neofetch shellcheck tree ubuntu-restricted-extras xclip
+sudo apt install -y bash-completion chrome-gnome-shell curl git htop jq keychain neofetch shellcheck tree ubuntu-restricted-extras xclip
 
 # fonts
 sudo apt install -y fonts-firacode ttf-mscorefonts-installer ttf-ancient-fonts
