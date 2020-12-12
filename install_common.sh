994a38004aa77c0fd0b4b73ef239443600e4e681
Nelson da Costa <ndacosta@octo.com>
2020-12-12 07:46:41 +0100

    forgot to install php at the same time as extensions in on liner

--- install_common.sh
+++ install_common.sh
@@ -24,7 +24,7 @@ sudo apt -y purge php.*
 sudo add-apt-repository -yes ppa:ondrej/php
 sudo apt-get update
 
-sudo apt install -y php8.0-{bcmath,cli,curl,intl,mbstring,pgsql,xml}
+sudo apt install -y php8.0 php8.0-{bcmath,cli,curl,intl,mbstring,pgsql,xml}
 
 sudo sed -i "s/;date.timezone =.*/date.timezone = Europe\/Paris/" /etc/php/8.0/apache2/php.ini
 sudo sed -i "s/;date.timezone =.*/date.timezone = Europe\/Paris/" /etc/php/8.0/cli/php.ini
