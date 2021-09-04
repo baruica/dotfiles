630e73b9794b0de737bbec6a781f944a8b59a954
Nelson da Costa <baruica@gmail.com>
2021-09-04 11:15:05 +0200

    use starship instead of git-radar

--- install.sh
+++ install.sh
@@ -5,14 +5,9 @@ readonly DIR=$(dirname "$(readlink -m "$0")")
 rm -f ~/.bash_profile
 cp "${DIR}"/.bash_profile_dist ~/.bash_profile
 
-rm -f ~/.bashrc
-ln -s "${DIR}"/.bashrc ~/.bashrc
-. ~/.bashrc
-
 git config --global user.name "Nelson da Costa"
 git config --global include.path "${DIR}"/git/.gitconfig
 
-[[ ! -d ~/bin ]] && mkdir ~/bin
-
-[[ ! -d ~/git-radar ]] && git clone https://github.com/michaeldfallen/git-radar ~/git-radar
-[[ ! -e ~/bin/git-radar ]] && ln -s ~/git-radar/git-radar ~/bin/git-radar
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+echo "eval \"$(starship init bash)\"" >> ~/.bashrc
+cp "${DIR}"/starship.toml ~/.config/
