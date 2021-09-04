212b324be6989f2a47c953c6cb0a2db7059a648f
Nelson da Costa <baruica@gmail.com>
2021-09-04 11:48:27 +0200

    use includeIf to switch between gitconfig files

--- install.sh
+++ install.sh
@@ -5,8 +5,9 @@ readonly DIR=$(dirname "$(readlink -m "$0")")
 rm -f ~/.bash_profile
 cp "${DIR}"/.bash_profile_dist ~/.bash_profile
 
-git config --global user.name "Nelson da Costa"
-git config --global include.path "${DIR}"/git/.gitconfig
+rm -f ~/.gitconfig
+cp "${DIR}"/git/baruica.gitconfig ~/baruica/
+cp "${DIR}"/git/dist.gitconfig ~/.gitconfig
 
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 echo "eval \"$(starship init bash)\"" >> ~/.bashrc
