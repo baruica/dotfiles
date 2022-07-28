8a362a55a067515bb9bab596c93f5d13d7a2531e
Nelson da Costa <baruica@gmail.com>
2022-07-28 09:38:25 +0200

    feat: add bat and exa to common install, and use exa for ll alias if installed

--- install_common.sh
+++ install_common.sh
@@ -3,4 +3,4 @@
 sudo apt remove -y apport apport-gtk
 
 sudo add-apt-repository -yes multiverse
-sudo apt install -y bash-completion blueman chrome-gnome-shell curl git htop jq keychain neofetch shellcheck tree ubuntu-restricted-extras xclip
+sudo apt install -y bash-completion bat blueman chrome-gnome-shell curl exa git htop jq keychain neofetch shellcheck tree ubuntu-restricted-extras xclip
