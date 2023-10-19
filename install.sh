9d12dda2236b9d988ba9d96a75be8fa8a68a8e6d
Nelson da Costa <baruica@gmail.com>
2023-10-19 12:15:06 +0200

    refactor: install cargo utils and merge install scripts

--- install.sh
+++ install.sh
@@ -9,6 +9,19 @@ rm -f ~/.gitconfig
 cp "${DIR}"/git/baruica.gitconfig ~/baruica/
 cp "${DIR}"/git/dist.gitconfig ~/.gitconfig
 
-sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+sudo apt remove -y apport apport-gtk
+
+sudo add-apt-repository -y multiverse
+sudo apt update
+
+sudo apt install -y curl git jq keychain neofetch shellcheck ubuntu-restricted-extras xclip cargo libssl-dev pkg-config
+
+cargo install cargo-update
+
+cargo install startship --locked
 echo "eval \"$(starship init bash)\"" >> ~/.bashrc
 cp "${DIR}"/starship.toml ~/.config/
+
+cargo install topgrade
+
+curl -s "https://get.sdkman.io" | bash
--- install_common.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt remove -y apport apport-gtk
-
-sudo add-apt-repository -yes multiverse
-sudo apt install -y bash-completion bat blueman chrome-gnome-shell curl exa git htop jq keychain neofetch shellcheck tree ubuntu-restricted-extras xclip
