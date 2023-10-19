4c638139b78dcbead5d1a95d3e180d17dbb8f82a
Nelson da Costa <baruica@gmail.com>
2023-10-19 12:19:17 +0200

    refactor: use cp -rf to avoid having to delete files as a prerequisite

--- install.sh
+++ install.sh
@@ -2,12 +2,10 @@
 
 readonly DIR=$(dirname "$(readlink -m "$0")")
 
-rm -f ~/.bash_profile
-cp "${DIR}"/.bash_profile_dist ~/.bash_profile
+cp -rf "${DIR}"/.bash_profile_dist ~/.bash_profile
 
-rm -f ~/.gitconfig
-cp "${DIR}"/git/baruica.gitconfig ~/baruica/
-cp "${DIR}"/git/dist.gitconfig ~/.gitconfig
+cp -rf "${DIR}"/git/baruica.gitconfig ~/baruica/
+cp -rf "${DIR}"/git/dist.gitconfig ~/.gitconfig
 
 sudo apt remove -y apport apport-gtk
 
@@ -20,7 +18,7 @@ cargo install cargo-update
 
 cargo install startship --locked
 echo "eval \"$(starship init bash)\"" >> ~/.bashrc
-cp "${DIR}"/starship.toml ~/.config/
+cp -rf "${DIR}"/starship.toml ~/.config/
 
 cargo install topgrade
 
